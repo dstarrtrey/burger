9cb0acf345a54a1c4d72286c80e00ec9ce8af377
dstarrtrey <dstarrtrey@gmail.com>
2019-03-25 20:15:00 -0700

    Adds dotenv, optimizes heroku database connection, and simplifies schema.sql

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db'
-});
+const connection = mysql.createConnection(
+  process.env.DATABASE_URL || {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  }
+);
 
 connection.connect(function(err) {
   if (err) {
--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,3 @@
-DROP DATABASE IF EXISTS burgers_db;
-
-CREATE DATABASE burgers_db;
-
-USE burgers_db;
-
 CREATE TABLE burgers (
 	id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
